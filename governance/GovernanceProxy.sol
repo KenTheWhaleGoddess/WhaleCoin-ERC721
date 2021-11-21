e7049ae0edf1bb80712fa59cac924dcda93ff3dc
Ken Preiser <kenpreiser@gmail.com>
2021-11-21 15:27:54 -0800

    Update GovernanceProxy.sol

--- governance/GovernanceProxy.sol
+++ governance/GovernanceProxy.sol
@@ -1,24 +1,46 @@
-// SPDX-License-Identifier: None
+pragma solidity ^0.8.6;
 
-pragma solidity ^0.8.7;
+contract Proxy {
+  bytes32 private constant _ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+  bytes32 private constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-import "@openzeppelin/contracts/proxy/Proxy.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-
-contract OnChainGovernanceProxy is Proxy, Ownable {
-    
-    address implementation;
-    
-    function _implementation() override internal view returns(address) {
-        return implementation;
+  constructor(address initialImpl) {
+    address _admin = msg.sender;
+    assembly {
+      sstore(_ADMIN_SLOT, _admin)
+      sstore(_IMPLEMENTATION_SLOT, initialImpl)
     }
-    
-    function changeImplementation(address _newImplementation) public onlyOwner {
-        implementation = _newImplementation;
+  }
+
+  function admin() public view returns (address adm) {
+    bytes32 slot = _ADMIN_SLOT;
+    assembly {
+      adm := sload(slot)
     }
-    fallback(bytes calldata) external override payable returns (bytes memory){
-        (, bytes memory data) = implementation.delegatecall(msg.data);
-        return data;
+  }
+
+  function implementation() public view returns (address impl) {
+    bytes32 slot = _IMPLEMENTATION_SLOT;
+    assembly {
+      impl := sload(slot)
     }
+  }
+
+  function upgrade(address newImplementation) external {
+    require(msg.sender == admin(), 'admin only');
+    bytes32 slot = _IMPLEMENTATION_SLOT;
+    assembly {
+      sstore(slot, newImplementation)
+    }
+  }
+
+  fallback() external payable {
+    assembly {
+      let _target := sload(_IMPLEMENTATION_SLOT)
+      calldatacopy(0x0, 0x0, calldatasize())
+      let result := delegatecall(gas(), _target, 0x0, calldatasize(), 0x0, 0)
+      returndatacopy(0x0, 0x0, returndatasize())
+      switch result case 0 {revert(0, 0)} default {return (0, returndatasize())}
+    }
+  }
 }
