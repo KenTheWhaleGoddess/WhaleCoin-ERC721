bdaf6d091ba4cfc215a7ae271a25c33ebd9d82c8
Ken Preiser <kenpreiser@gmail.com>
2022-02-27 13:12:02 -0800

    Update CanOwnTokens.sol

--- CanOwnTokens.sol
+++ CanOwnTokens.sol
@@ -17,11 +17,19 @@ contract NFTsThatCanOwnTokens is ERC721, Ownable, ReentrancyGuard {
   mapping(uint256 => mapping(ERC20 => uint256)) balances;
   mapping(uint256 => EnumerableSet.AddressSet) tokensInNFT;
   mapping(ERC20 => bool) approvedTokens;    
-  constructor() ERC721("NFT Cash Card", "$$$") {}
+  mapping(address => bool) frens;
+  constructor() ERC721("NFT Cash Card V1", "$$$") {
+    frens[msg.sender] = true;
+  }
 
   modifier onlyOwnerOf(uint256 tokenId) {
     require(_exists(tokenId), "Token not in existence");
     require(_isApprovedOrOwner(msg.sender, tokenId), "Token not owned by caller");
+    _;
+  }
+
+  modifier onlyFrens() {
+    require(frens[msg.sender], "not a fren");
     _;
   }
 
@@ -31,12 +39,10 @@ contract NFTsThatCanOwnTokens is ERC721, Ownable, ReentrancyGuard {
   }
 
   // public
-  function mint(uint256 _count) public payable {
-    require(counter + _count <= 10000);
+  function mint(uint256 _count) public {
+    require(counter + _count <= 1000);
+    require(_count < 11, "10 is the max at a time");
 
-    if (msg.sender != owner()) {
-      require(msg.value >= (0.01 ether) * _count);
-    }
     for(uint256 i = 0; i < _count; i++) {
       _safeMint(msg.sender, counter + i);
     }
@@ -69,7 +75,7 @@ contract NFTsThatCanOwnTokens is ERC721, Ownable, ReentrancyGuard {
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
         require(_exists(tokenId),"ERC721Metadata: URI query for nonexistent token");
         string memory parts;
-        parts = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: fgHue; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill=bgHue/><text x="10" y="20" class="base">';
+        parts = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="0x542B72"/><text x="10" y="20" class="base">';
 
         for (uint256 i = 0; i < tokensInNFT[tokenId].length(); i++) {
           ERC20 token = ERC20(tokensInNFT[tokenId].at(i));
@@ -78,20 +84,20 @@ contract NFTsThatCanOwnTokens is ERC721, Ownable, ReentrancyGuard {
 
         string memory output = string(abi.encodePacked(parts, '</text></svg>'));
 
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "NFT Cash Card #', tokenId.toString(), '", "description": "This NFT cash card is a fully on-chain representation of assets managed by the NFT smart contract. NFTs have balances, can be withdrawn from by the owner.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "NFT Cash Card #', tokenId.toString(), ' (V1)", "description": "This NFT cash card is a fully on-chain representation of assets managed by the NFT smart contract. NFTs have balances, can be withdrawn from by the owner. It currently holds ', tokensInNFT[tokenId].length().toString(), ' types of ERC20s. It is made as a free mint to demonstrate NFT Utility.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
     }
 
-  function toggleApprovedToken(ERC20 token) public onlyOwner {
+  function toggleApprovedToken(ERC20 token) public onlyFrens {
     approvedTokens[token] = !approvedTokens[token];
   }
 
-  function withdraw() public payable onlyOwner {
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
+  function toggleFren(address _fren) public onlyOwner {
+    frens[_fren] = !frens[_fren];
+  }
+  function isFren(address _fren) public view returns (bool) {
+    return frens[_fren];
   }
 }
